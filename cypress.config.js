9457ae54716069de60a9bc14976e600d9ae7701a
cypressdeveloper85 <116920071+cypressdeveloper85@users.noreply.github.com>
2022-11-14 15:19:33 -0800

    Revert "Third commit"
    This reverts commit c4e0a850afbfbfdb36e79c5f2beb8fda229a4bc9.

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  projectId: 'vamipv',
+  projectId: '7fqnmu',
   e2e: {
     experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
